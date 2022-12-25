93f72b968a01b8e15890dc1c6b2e242d46ac7071
Youn Se Eun <tpdms717@gmail.com>
2022-12-25 23:27:50 +0900

    🧪 Add failing tests with boundary conditions

--- Province.test.js
+++ Province.test.js
@@ -18,4 +18,54 @@ describe('province', () => {
     expect(asia.shortfall).toEqual(-6)
     expect(asia.profit).toEqual(292)
   })
+
+  it('zero demand', () => {
+    asia.demand = 0
+    expect(asia.shortfall).toEqual(-25)
+    expect(asia.profit).toEqual(0)
+  })
+
+  it('negative demand', () => {
+    asia.demand = -1
+    expect(asia.shortfall).toEqual(-26)
+    expect(asia.profit).toEqual(-10)
+  })
+
+  it('empty demand', () => {
+    asia.demand = ''
+    expect(asia.shortfall).toBeNaN()
+    expect(asia.profit).toBeNaN()
+  })
+})
+
+describe('no producers', () => {
+  let noProducers
+  beforeEach(() => {
+    const data = {
+      name: 'No Producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    }
+    noProducers = new Province(data)
+  })
+  it('shortfall', () => {
+    expect(noProducers.shortfall).toEqual(30)
+  })
+  it('profit', () => {
+    expect(noProducers.profit).toEqual(0)
+  })
+})
+
+describe('string for producers', () => {
+  it('', () => {
+    const data = {
+      name: 'String',
+      producers: '',
+      demand: 30,
+      price: 20,
+    }
+    const prov = new Province(data)
+    expect(prov.shortfall).toEqual(0)
+  })
 })
